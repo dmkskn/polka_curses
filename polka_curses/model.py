d56366b3c9a63297515b5ca769f001f22b3524f9
dmkskn <dmksknn@gmail.com>
2019-07-16 22:31:25 +0300

    Add Model

--- polka_curses/model.py
+++ polka_curses/model.py
@@ -1,2 +1,58 @@
+import polka
+
+
 class Model:
-    pass
+    """Access to the Polka API"""
+
+    def __init__(self):
+        self._books = []
+        self._lists = []
+        self._experts = []
+        self.get_all()
+
+    @property
+    def books(self):
+        """Gets list of books. Each book is an instance of polka.Book
+        class."""
+        if not self._books:
+            self._books = polka.books()
+        return self._books
+
+    @property
+    def lists(self):
+        """Gets list of compilations. Each compilations is an instance
+        of polka.Compilation class."""
+        if not self._lists:
+            self._lists = polka.lists()
+        return self._lists
+
+    @property
+    def experts(self):
+        """Gets list of experts. Each expert is an instance
+        of polka.Pundit class."""
+        if not self._experts:
+            self._experts = polka.pundits()
+        return self._experts
+
+    def search(self, query):
+        """Search for the `query`. Each expert is a tuple `(title,
+        description, object)`."""
+        return polka.search(query)
+
+    def is_book(self, obj):
+        return isinstance(obj, polka.Book)
+
+    def is_list(self, obj):
+        return isinstance(obj, polka.Compilation)
+
+    def is_expert(self, obj):
+        return isinstance(obj, polka.Pundit)
+
+    def book_has_article(self, book: polka.Book):
+        return book.has_article
+
+    def get_all(self):
+        """Preload all data"""
+        _ = self.books
+        _ = self.lists
+        _ = self.experts
--- tests/test_model.py
+++ tests/test_model.py
@@ -1,2 +1,83 @@
 import pytest
+import polka
+import random
+
 from polka_curses.model import Model
+
+
+BOOK_ID = 523
+LIST_ID = 85
+EXPERT_ID = 344
+
+
+@pytest.fixture
+def model():
+    return Model()
+
+
+@pytest.fixture
+def book():
+    return polka.Book(BOOK_ID)
+
+
+@pytest.fixture
+def not_book():
+    return random.choice([polka.Compilation(LIST_ID), polka.Pundit(EXPERT_ID)])
+
+
+@pytest.fixture
+def expert():
+    return polka.Pundit(EXPERT_ID)
+
+
+@pytest.fixture
+def not_expert():
+    return random.choice([polka.Compilation(LIST_ID), polka.Book(BOOK_ID)])
+
+
+@pytest.fixture
+def list_():
+    return polka.Compilation(LIST_ID)
+
+
+@pytest.fixture
+def not_list():
+    return random.choice([polka.Pundit(EXPERT_ID), polka.Book(BOOK_ID)])
+
+
+def test_get_books(model):
+    assert [book is polka.Book for book in model.books]
+
+
+def test_get_lists(model):
+    assert [l is polka.Compilation for l in model.lists]
+
+
+def test_get_experts(model):
+    assert [expert is polka.Pundit for expert in model.experts]
+
+
+def test_search(model):
+    result, *results = polka.search("пушкин")
+    assert isinstance(results, list)
+    assert isinstance(result, tuple)
+    assert len(result) == 3
+
+
+def test_book_is_book(model, book, not_book):
+    assert model.is_book(book)
+    assert not model.is_book(not_book)
+
+
+def test_expert_is_expert(model, expert, not_expert):
+    assert model.is_expert(expert)
+    assert not model.is_expert(not_expert)
+
+
+def test_list_is_list(model, list_, not_list):
+    assert model.is_list(list_)
+    assert not model.is_list(not_list)
+
+
+def test_book_has_article(model, book):
+    assert model.book_has_article(book) == book.has_article

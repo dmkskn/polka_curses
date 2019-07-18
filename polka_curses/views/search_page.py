c24a78625ff7260f0b9b1b094b36894547713075
dmkskn <dmksknn@gmail.com>
2019-07-18 16:39:06 +0300

    Add SearchPage

---
+++ polka_curses/views/search_page.py
@@ -0,0 +1,67 @@
+import urwid
+from urwid import CENTER, TOP
+
+
+RELATIVE_55 = ("relative", 55)
+RELATIVE_90 = ("relative", 90)
+
+
+class SearchResultsExseption(Exception):
+    pass
+
+
+class SearchPage(urwid.WidgetWrap):
+    def __init__(self):
+        self.search = urwid.Edit()
+        bg = urwid.SolidFill("⢻")
+        w = urwid.Filler(urwid.LineBox(self.search, "ПОИСК 🔎", CENTER))
+        w = urwid.Overlay(w, bg, CENTER, RELATIVE_55, TOP, 3, top=2)
+        super().__init__(w)
+
+    def get_search_query(self):
+        return self.search.edit_text
+
+
+class SearchResultsPage(urwid.WidgetWrap):
+    def __init__(self, results):
+        self.results = SearchResults(results)
+        bg = urwid.SolidFill("⢻")
+        w = urwid.LineBox(self.results, "ПОИСК 🔎", CENTER)
+        w = urwid.Overlay(w, bg, CENTER, RELATIVE_55, TOP, RELATIVE_90, top=2)
+        super().__init__(w)
+
+    def get_focused_result(self):
+        return self.results.get_focused_result()
+
+
+class SearchResults(urwid.ListBox):
+    def __init__(self, results):
+        self.results = [SearchResultItem(r) for r in results]
+        super().__init__(urwid.SimpleFocusListWalker(self.results))
+
+    def get_focused_result(self):
+        if self.results:
+            w, _ = self.get_focus()
+            return w.result.object
+        else:
+            raise SearchResultsExseption("there is no results")
+
+
+class SearchResultItem(urwid.WidgetWrap):
+    def __init__(self, result):
+        self.result = result
+        body = urwid.Text(self.body)
+        body = urwid.Padding(body, left=1, right=1)
+        item = urwid.LineBox(body, self.header, urwid.LEFT)
+        super().__init__(urwid.AttrMap(item, "frame", "highlighted"))
+
+    @property
+    def header(self):
+        return self.result.title
+
+    @property
+    def body(self):
+        return f"{self.result.description}..."
+
+    def selectable(self):
+        return True
---
+++ tests/test_views/test_search_page.py
@@ -0,0 +1,44 @@
+import pytest
+import polka
+
+from polka_curses.views.search_page import (
+    SearchPage,
+    SearchResultsPage,
+    SearchResults,
+    SearchResultItem,
+)
+
+
+@pytest.fixture
+def search_page():
+    return SearchPage()
+
+
+@pytest.fixture
+def search_results_page():
+    return SearchResultsPage(polka.search("грибоедов"))
+
+
+@pytest.fixture
+def search_results(search_results_page):
+    return search_results_page.results
+
+
+@pytest.fixture
+def search_result_item(search_results):
+    return search_results.results[0]
+
+
+def test_get_search_query(search_page):
+    assert search_page.get_search_query() == ""
+    search_page.search.edit_text = "abc"
+    assert search_page.get_search_query() == "abc"
+
+
+def test_get_focused_result(search_results):
+    focused_object = search_results.get_focused_result()
+    assert search_results.results[0].result.object == focused_object
+
+
+def test_search_result_item_is_selectable(search_result_item):
+    assert search_result_item.selectable()

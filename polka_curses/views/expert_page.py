0f32a9cdbcb714ebbddfc4daff3a185a3146b34a
dmkskn <dmksknn@gmail.com>
2019-07-20 14:50:57 +0300

    Add ExpertPage

---
+++ polka_curses/views/expert_page.py
@@ -0,0 +1,55 @@
+import urwid
+
+from .books_page import BooksPage
+
+
+class ExpertPage(urwid.WidgetWrap):
+    def __init__(self, expert):
+        self.expert = expert
+        self.is_two_columns = expert.favorites and expert.wrote_about
+        self.favorites = self.build_favorites()  # type: BooksPage
+        self.wrote_about = self.build_wrote_about()  # type: BooksPage
+        self.columns = {0: self.favorites, 1: self.wrote_about}
+        super().__init__(self.build())
+
+    def build(self):
+        if self.is_two_columns:
+            favorites = urwid.LineBox(self.favorites, title="ВЫБОР ЭКСПЕРТА")
+            wrote_about = urwid.LineBox(self.wrote_about, title="СТАТЬИ")
+            self.container = urwid.Columns([favorites, wrote_about], 1, 0)
+        elif self.favorites:
+            favorites = urwid.LineBox(self.favorites, title="ВЫБОР ЭКСПЕРТА")
+            self.container = favorites
+        elif self.wrote_about:
+            wrote_about = urwid.LineBox(self.wrote_about, title="СТАТЬИ")
+            self.container = wrote_about
+        else:
+            self.container = urwid.Filler(urwid.Text(""))
+        return urwid.Pile([("pack", self.description), self.container])
+
+    def build_favorites(self):
+        if self.expert.favorites:
+            return BooksPage(self.expert.favorites)
+        else:
+            return None
+
+    def build_wrote_about(self):
+        if self.expert.wrote_about:
+            return BooksPage(self.expert.wrote_about)
+        else:
+            return None
+
+    @property
+    def description(self):
+        desc = urwid.Text(self.expert.description)
+        desc = urwid.Padding(desc, left=1, right=1)
+        return urwid.LineBox(desc)
+
+    def get_focused_book(self):
+        column = self.get_focused_column()
+        return column.get_focused_book()
+
+    def get_focused_column(self):
+        if self.is_two_columns:
+            return self.columns[self.container.focus_position]
+        return self.favorites or self.wrote_about
---
+++ tests/test_views/test_expert_page.py
@@ -0,0 +1,75 @@
+import pytest
+
+from polka_curses.views.expert_page import ExpertPage
+from polka_curses.model import Model
+
+
+model = Model()
+
+
+@pytest.fixture
+def expert_page_full():
+    """Has favorite books and wrote articles"""
+    experts = [e for e in model.experts if e.favorites and e.wrote_about]
+    if experts:
+        return ExpertPage(experts[0])
+    else:
+        return None
+
+
+@pytest.fixture
+def expert_page_favs():
+    """Only favorite books. Wrote no articles."""
+    experts = [e for e in model.experts if e.favorites and not e.wrote_about]
+    if experts:
+        return ExpertPage(experts[0])
+    else:
+        return None
+
+
+@pytest.fixture
+def expert_page_wrote():
+    """No favorite books. Wrote articles."""
+    experts = [e for e in model.experts if not e.favorites and e.wrote_about]
+    if experts:
+        return ExpertPage(experts[0])
+    else:
+        return None
+
+
+def test_is_two_columns_arg(expert_page_full, expert_page_favs):
+    if expert_page_full:
+        assert expert_page_full.is_two_columns
+    if expert_page_favs:
+        assert not expert_page_favs.is_two_columns
+
+
+def test_get_focused_column_when_two_of_them(expert_page_full):
+    if expert_page_full:
+        expert = expert_page_full
+        assert expert.get_focused_column() is expert.favorites
+        expert.container.focus_position = 1
+        assert expert.get_focused_column() is expert.wrote_about
+
+
+def test_get_focused_column_when_only_one(expert_page_favs, expert_page_wrote):
+    e1, e2 = expert_page_favs, expert_page_wrote
+    if e1:
+        assert e1.get_focused_column() is e1.favorites
+    if e2:
+        assert e2.get_focused_column() is e2.wrote_about
+
+
+def test_get_focused_book_if_expert_is_full(expert_page_full):
+    if expert_page_full:
+        assert model.is_book(expert_page_full.get_focused_book())
+
+
+def test_get_focused_book_if_expert_has_only_favs(expert_page_favs):
+    if expert_page_favs:
+        assert model.is_book(expert_page_favs.get_focused_book())
+
+
+def test_get_focused_book_if_expert_only_wrote(expert_page_wrote):
+    if expert_page_wrote:
+        assert model.is_book(expert_page_wrote.get_focused_book())
